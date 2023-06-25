0e1d87ea0d2cf99e3e0db4fdc10f5f81d2f9c8e9
Sergei Solovev <ssseee580@gmail.com>
2023-06-25 23:33:57 +0300

    Adding functionality

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+# Очистка экрана перед установкой
+clear
+
+echo "
+[EN] Preparing the server for automatic configuration.
+[RU] Подготовка сервера к автоматической настройке.
+"
+
+# Обновление таблиц
+apt update -y >> "$(dirname "$0")/enginegp_install.log" 2>&1
+
+# Обновление пакетов
+apt upgrade -y >> "$(dirname "$0")/enginegp_install.log" 2>&1
+
 # Установка необходимых пакетоа
-apt install sudo whiptail dialog -y
+apt-get install sudo whiptail dialog dos2unix -y >> "$(dirname "$0")/enginegp_install.log" 2>&1
+
+# Преобразование установщика в unix
+dos2unix deb.install.sh >> "$(dirname "$0")/enginegp_install.log" 2>&1
 
 # Делаем файл "deb.install.sh" исполняемым и запускаем
 chmod +x deb.install.sh && ./deb.install.sh
